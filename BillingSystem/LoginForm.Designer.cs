3756b15f3e81ce3aa8778a80c420e7de29af3594
Romaios <romeosy.12bgn@gmail.com>
2026-06-11 09:53:57 +0800

    Updated LoginForm title to Billing System v1.0 - Login

--- BillingSystem/LoginForm.Designer.cs
+++ BillingSystem/LoginForm.Designer.cs
@@ -119,7 +119,7 @@
             MaximizeBox = false;
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Billing System - Login";
+            Text = "Billing System v1.0 - Login";
             ResumeLayout(false);
             PerformLayout();
         }
